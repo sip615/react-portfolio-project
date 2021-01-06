64da1e7169bb1c2dd6da788e78671bb220ac84c8
sip615 <sunilpatel.328@gmail.com>
2021-01-06 01:09:23 -0500

    fixed the hamburger dropdown issue

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,15 +5,15 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-dark">
             <div className="container">
-                <div className="container-fluid">
+                {/* <div className="container-fluid"> */}
                     <a className="navbar-brand" href="#"><img className="logo" src={logo} alt="logo..."></img></a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                        <ul className="navbar-nav ml-auto">
+                            <li className="nav-item active">
+                                <a className="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
                             </li>
                             <li className="nav-item">
                                 <a className="nav-link" href="#">About ME</a>
@@ -26,7 +26,7 @@ const Navbar = () => {
                             </li>
                         </ul>
                     </div>
-                </div>
+                {/* </div> */}
             </div>
         </nav>
     )
