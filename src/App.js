ebf303e5c3516f18a25fcef06ef4dc595f456542
sip615 <sunilpatel.328@gmail.com>
2021-01-09 22:43:35 -0500

    Header complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Particles from "react-particles-js";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
+import { faCircle } from "@fortawesome/free-solid-svg-icons";
+
 
 function App() {
   return (
     <>
-    <Navbar/>
-    <Header/>
+      <Particles
+        className="particles-canvas"
+        params={
+          {
+            particles:{
+              number:{
+                value: 30,
+                density: {
+                  enable: true,
+                  value_area: 900
+                }
+              },
+              shape:{
+                type: "circle",
+                stroke:{
+                  width: 3,
+                  color: "#38f9fc"
+                }
+              }
+            }
+          }
+        }
+      />
+      <Navbar/>
+      <Header/>
     </>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,19 @@
 import React from "react";
+import Typed from "react-typed";
 
 const Header = () => {
     return (
         <div className="header-wrapper">
             <div className="main-info">
                 <h1>Web Development Projects</h1> 
+                <Typed 
+                    className="typed-text" 
+                    strings={["Web Development", "Data Science", "SEO"]}
+                    typeSpeed={40}
+                    backSpeed={60}
+                    loop
+                />
+                <a href="#" className="btn-main-offer">Contact Me</a>
             </div>
         </div>
     )
