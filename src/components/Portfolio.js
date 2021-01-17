2d206e1fbbb0ca531e0d591589b252a15a4bbf4b
sip615 <sunilpatel.328@gmail.com>
2021-01-16 23:20:10 -0500

    started portoflio component

---
+++ src/components/Portfolio.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Portfolio = () => {
+    return (
+        <div className="portfolio-wrapper">
+            <div className="container">
+                <ht className="text-uppercase text-center py-5">portfolio</ht>
+                <div className="image-box-wrapper row justify-content-">
+                    <img className="portfolio-image" src="" alt=""/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Portfolio
